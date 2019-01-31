45a4a0e7668079728fa8c99d7dd95704ba72cbb2
dhakalmukesh07 <dhakalmukesh07@gmail.com>
2019-01-31 15:54:27 +0545

    First Commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def register
+  end
+
+  def help
+  end
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,4 @@
+lass Micropost < ApplicationRecord
+belongs_to :user
+validates :content, length: { maximum: 140 }, presence: true
+end
